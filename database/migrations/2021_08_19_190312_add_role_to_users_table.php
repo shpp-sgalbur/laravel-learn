e23ad6f1fd6dc2fd655796f59363fca116366dc6
Galbur Sergey <gal_sergey@ukr.net>
2021-08-20 19:10:37 +0300

    add role field to users table

--- database/migrations/2021_08_19_190312_add_role_to_users_table.php
+++ database/migrations/2021_08_19_190312_add_role_to_users_table.php
@@ -12,7 +12,7 @@ class AddRoleToUsersTable extends Migration
      * @return void
      */
     public function up()
-    {
+    {               
         Schema::table('users', function (Blueprint $table) {
             $table->string('role')->default('User');
         });
