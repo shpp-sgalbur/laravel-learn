0e048168f2e52b515bdeafa4370bf7df530f7239
Galbur Sergey <gal_sergey@ukr.net>
2021-08-20 18:56:43 +0300

    add superuser to users table

--- database/migrations/2021_08_19_190312_add_role_to_users_table.php
+++ database/migrations/2021_08_19_190312_add_role_to_users_table.php
@@ -15,6 +15,7 @@ class AddRoleToUsersTable extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             $table->string('role')->default('User');
+            
         });
     }
 
