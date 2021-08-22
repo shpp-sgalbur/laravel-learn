445f4cbaa79b5abdf32e3c2b7c6e87b66a69204c
Galbur Sergey <gal_sergey@ukr.net>
2021-08-22 19:21:25 +0300

    created routes for BlogController

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,6 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
+Route::resource('blogs', \App\Http\Controllers\BlogController::class);
+
 require __DIR__.'/auth.php';
