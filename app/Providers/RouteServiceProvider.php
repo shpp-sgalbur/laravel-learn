545a014b1ed8bbdb5a5793ecaf162f78b4363605
Galbur Sergey <gal_sergey@ukr.net>
2021-08-27 21:23:13 +0300

    set up redirect to homepage after login

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/';
+    public const HOME = '/blogs';
 
     /**
      * The controller namespace for the application.
